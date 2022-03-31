9a89b0b67468fef60928b0749112187d669aa132
Toni <toni@MacBook-Pro-de-Voicemod.local>
2022-03-31 20:56:41 +0200

    Updated endpoint to get mor accurate precition

--- src/app/client/region/regionClient.js
+++ src/app/client/region/regionClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getWeatherInfoByRegion = async (region) => {
-    const { data } = await axios.get(`${process.env.VITE_API_BASE_URL}/prediccion/ccaa/hoy/${region}?api_key=${process.env.VITE_API_KEY}`);
+    const { data } = await axios.get(`${process.env.VITE_API_BASE_URL}/prediccion/ccaa/manana/${region}?api_key=${process.env.VITE_API_KEY}`);
     const { datos: infoURL } = data;
 
     const { data: textInfo} = await axios.get(infoURL);
